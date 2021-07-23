6f1280fa738187999a06d97b31cd1e85202bde93
wwz3 <62084144+wwz3@users.noreply.github.com>
2021-07-23 15:30:40 +0800

    Update
    change code

---
+++ Packages/Assets/WinnerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinnerScript : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //text.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.layer == 7)
+        //{
+            text.SetActive(true);
+            GetComponent<Text> ().color = Color.black;
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        text.SetActive(false); 
+        Destroy(cube);
+    }
+}
